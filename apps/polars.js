301fdad95b628ee3edf9a3dd1c6df1b337819d05
Grady Morgan <gradymorgan@gmail.com>
2014-04-07 22:24:56 -0700

    first pass at polars: untested

---
+++ apps/polars.js
@@ -0,0 +1,117 @@
+//! polars.js
+//! calculate polar targets based on tws.
+//! version : 0.1
+//! homegrownmarine.com
+
+
+var fs = require('fs');
+var readline = require('readline');
+
+var _ = require('lodash');
+
+var loading = false;
+
+function polarTable() {
+    this.all = {};
+    this.targets = {'up': {}, 'down': {}};
+}
+
+polarTable.prototype.getInterpolatedValue = function(tws, key, upwind) {
+    var appropriateTargets = this.targets[upwind?'up':'down'];
+    var twspeeds = _.keys(appropriateTargets);
+
+    var found = [];
+    for (var i=1; i < twspeeds.length; i++) {
+        if ( tws < twspeeds[i] ) {
+            found[1] = twspeeds[i][key];
+            found[0] = twspeeds[i-1][key];
+            break;
+        }
+    }
+
+    var percentFirst = (tws-found[0]) / (found[1]-found[0]);
+    var interpolatedValue = percentFirst * found[0] + (1-percentFirst) * found[1];
+    return interpolatedValue;    
+}
+
+polarTable.prototype.targetSpeed = function(tws, upwind) {
+    return this.getInterpolatedValue(tws, 'speed', upwind);
+}
+
+polarTable.prototype.targetAngle = function(tws, upwind) {
+    return this.getInterpolatedValue(tws, 'twa', upwind);
+}
+
+polarTable.prototype.targetHeel = function(tws, upwind) {
+    return this.getInterpolatedValue(tws, 'heel', upwind);
+}
+
+//creates polarTable from filename
+polarTable.fromFile = function(filename, callback) {
+
+    var polars = new polarTable();
+
+    var rd = readline.createInterface({
+        input: fs.createReadStream(filename),
+        output: process.stdout,
+        terminal: false
+    });
+
+    rd.on('line', function(line) {
+        line = line.trim();
+        if ( line.length === 0 || line.indexOf('#') === 0 ) {
+            return;
+        }
+
+        // TWS TWA VMG Heel Lee
+        var target = false;
+        var upwind = true;
+
+        // if the line ends in a *, it's a target
+        // for this wind speed
+        if (line.substring(line.length-1) == '*') {
+            target = true;
+            line = line.substring(0,line.length-1);
+        }
+
+
+        var cols = line.split(' ');  //TODO: CSV
+        var tws = +cols[0],
+            twa = +cols[1],
+            speed = +cols[2],
+            heel = +cols[3],
+            lee = +cols[4];
+
+
+        if ( !(tws in polars.all) ) {
+            polars.all[tws] = {};
+        }
+
+        polars.all[tws][twa] = {speed: speed, heel: heel, lee: lee};
+        polars.targets[upwind?'up':'down'][tws] = {twa: twa, speed: speed, heel: heel};
+    });
+
+    rd.on('close', function() {
+        callback(polars);
+    });
+}
+
+
+exports.load = function(server, boat_data, settings) {
+    var filename = settings.get('polars:table');
+    if ( filename ) {
+        polarTable.fromFile(filename, function(polars) {
+            setInterval(function() {
+                var now = boat_data.current();
+                var upwind = Math.abs(now.twa) < 90;
+                var targets = {
+                    targetSpeed: polars.targetSpeed(now.tws, upwind),
+                    targetAngle: polars.targetAngle(now.tws, upwind),
+                    targetHeel: polars.targetHeel(now.tws, upwind)
+                };
+
+                boat_data.broadcast(null, targets);
+            }, 1000);
+        });
+    }
+};
---
+++ test/polars.js
@@ -0,0 +1,16 @@
+var assert = require('chai').assert;
+var moment = require('moment');
+
+var polars = require('../modules/polars.js');
+
+describe('polars', function() {
+    descripe('#fromFile', function() {
+        it(' should load', function(done) {
+
+        });
+    });
+
+    it(' should find speed.', function() {
+
+    });
+});
