463953c225947fdda70a853a46e9614a910a1d6c
Grady Morgan <gradymorgan@gmail.com>
2014-11-02 10:03:24 -0800

    calculate true wind and vmg

--- apps/calcs.js
+++ apps/calcs.js
@@ -4,61 +4,45 @@
 //! version : .1
 //! homegrownmarine.com
 
-// CURRENTLY DISABLED :: untested, provided as example
+var _ = require('lodash')
 
-var deg = exports.deg = function deg(radians) {
-    return (radians*180/Math.PI + 360) % 360;
-}    
-
-var rad = exports.rad = function rad(degrees) {
-    return degrees * Math.PI / 180;
-}
-
-//the current is the difference of the speed and heading over ground
-//and the speed and heading over water
-var calcCurrents = function(speed, hdg, sog, cog) {
-    //GM: TODO: understand 90 deg offset.
-    //convert cog and hdg to radians, with north right
-    hdg = rad(90.0 - hdg);
-    cog = rad(90.0 - cog);
-
-    //break out x and y components of current vector
-    var current_x = sog * Math.cos(cog) - speed * Math.cos(hdg);
-    var current_y = sog * Math.sin(cog) - speed * Math.sin(hdg);
-
-    //drift is the magnitude of the current vector
-    var drift = Math.sqrt(current_x * current_x + current_y * current_y);
-    
-    //set is the angle of the current vector (note we special case pure North or South)
-    var set = 0;
-    if ( current_x === 0 ) {
-        set = curr_y < 0? 180: 0;
-    }
-    else {
-        //normalize 0 - 360
-        set = (90.0 - deg(Math.atan2(current_y, current_x)) + 360) % 360;
-    }
-
-    return { set: set, drift: drift };
-};
+var calcs = require('homegrown-sailing').Calcs;
+var delayedInputs = require('homegrown-sailing').Utilities.delayedInputs;
 
 exports.load = function(server, boatData, settings) {
-    //@ 1Hz, broadcast current set and drift
-    setInterval(function() {
-        //first, get the latest boat data
-        var data = boatData.now();
-        
-        if ( isNaN(data.speed*data.hdg*data.sog*data.cog) ) return;
+    console.info("calcs:loaded")
 
-        //then use it to calculate the current vector
-        var currents = calcCurrents( data.speed, data.hdg, data.sog, data.cog );
+    var functions = [
+        //TODO: how to ignore system tws and direction?
+        delayedInputs(calcs.tws),
+        delayedInputs(calcs.twa),
+        delayedInputs(calcs.twd),
+        delayedInputs(calcs.vmg)
+    ];
+    var setX = delayedInputs(calcs.set);
+    var driftX = delayedInputs(calcs.drift);
 
-        //broadcast the new bit of data. boatData will handle
-        //NMEA formatting.
-        boatData.broadcast({
-            type: 'vdr',
-            set: currents.set,
-            drift: currents.drift
+    boatData.on('data', function(data) {
+        var syntheticData = {};
+
+        _.each(functions, function(f) {
+            var result = f(_.assign({}, data, syntheticData));
+
+            if (result) {
+                _.extend(syntheticData, result);
+            }
         });
-    }, 1000);
+
+        if ( _.size(syntheticData) ) {
+            boatData.broadcast(syntheticData);
+        }
+
+        var set = setX(data);
+        var drift = driftX(data);
+        if (set) {
+            //broadcast the new bit of data. boatData will handle
+            //NMEA formatting.
+            boatData.broadcast(_.extend({type: 'vdr'}, set, drift));
+        }
+    });
 };
