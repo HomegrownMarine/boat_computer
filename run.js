c794c7fd4c5c5bb73d895288ea65f321613fa183
Grady Morgan <gradymorgan@gmail.com>
2015-10-18 14:42:26 -0700

    cleanup main file

--- run.js
+++ run.js
@@ -68,110 +68,130 @@ function initializeWinston(winston, settings) {
         maxFiles: 10
     });
 
+    winston.info("starting boat_computer: PID "+process.pid);
     winston.info('logging to dir: ' + settings.get('winston:logDir')+'run.log');
     //winston.handleExceptions();
 }
 
+function initializeBoatData(sources, syncSystemTime) {
+    // boat data module
+    var boatData = new BoatData(sources);
+    boatData.start();
+
+    if ( syncSystemTime ) {
+        function setSystemTime() {
+            boatData.once('data:rmc', function(data) {
+                var now = data['time'];
+
+                winston.info('Syncing clock to GPS time :' + data['time'].format("YYYYMMDDmmhhss"));
+
+                exec('date +%s -s "@' + now.unix() + '"' );
+
+                // fs.exists('/etc/timestamp', function (exists) {
+                //     if (exists) {
+                //         exec('echo "' + now.format("YYYYMMDDmmhhss") +'" >>/etc/timestamp' );
+                //     }
+                // });
+                
+            });
+        };
+
+        //NOTE: changing the system time can give node JS troubles, especially changing it to the past
+        //after some seconds, to give all serial ports time to establish their connections
+        //change the system time to match the GPS provided time
+        setTimeout(setSystemTime, 30000);
+    }   
+
+    return boatData;
+}
+
+function initializeWebserver() {
+    var server = express();
+    server.use(express.urlencoded());
+    server.use(express.multipart());
+    //TODO: connection logging
+    //TODO: update express version and use more focused file uploading
+
+
+    //global libs
+    //TODO: less & compression middleware
+    server.use('/', express.static(path.join(__dirname, 'www')));
+
+    // start up webserver
+    server.set('port', settings.get('port'));
+    var server = server.listen(server.get('port'), function() {
+        winston.info('Express server listening on port ' + server.address().port);
+    });
+
+    return server;
+}
+
+function addServerDataRoutes(server, boatData) {
+    //returns current set of data for boat
+    server.get('/now', function(req, res) {
+        res.send(boatData.now());
+    });
+
+
+    //TODO: ES library
+    var streamConnections = [];
+    //on data, send to client
+    boatData.on('data', function streamOnData(data) {
+        _.each(streamConnections, function(res) {
+            res.write('id: ' + (new Date().getTime()) + '\n');
+            res.write('data: ' + JSON.stringify(data) + '\n\n');
+        });
+    });
+
+    //returns json package for each new piece of data on channel
+    server.get('/stream', function(req, res) {
+        //let request last forever
+        req.socket.setTimeout(Infinity);
+        
+        //set response type to SSE
+        res.writeHead(200, {
+            'Content-Type': 'text/event-stream',
+            'Cache-Control': 'no-cache',
+            'Connection': 'keep-alive'
+        });
+        res.write('\n');
+
+        streamConnections.push(res);
+
+        //on connection close, remove from list of active streams
+        req.on('close', function() {
+            streamConnections = _.without(streamConnections, res);
+        });
+    });
+}
+
+function addServerIndexPage(server, title, apps) {
+    //used to make list of loaded apps on index page
+    server.get('/', function(req, res) {
+        //todo: consider using express's template engine for this.
+        var index = handlebars.compile(fs.readFileSync(path.join(__dirname,'www/templates/index.html'), {encoding:'utf8'}));
+        
+        res.send( index({boatName: title, links: apps}) );
+    });
+}
+
 
 /// MAIN
-
 initializeWinston(winston, settings);
 
-winston.info("starting boat_computer: PID "+process.pid);
-
 //set up modules
+var boatData = initializeBoatData(settings.get('dataSources'), settings.get('syncSystemTime'));
 
-// boat data module
-var boatData = new BoatData(settings.get('dataSources'));
-boatData.start();
+var server = initializeWebserver();
 
-var server = express();
-server.use(express.urlencoded());
-server.use(express.multipart());
-//TODO: connection logging
-//TODO: update express version and use more focused file uploading
+//load apps after 
+setTimeout(function() {
+    //load installed apps
+    var apps = loadApps(server, boatData, settings);
 
-
-//global libs
-//TODO: less & compression middleware
-server.use('/', express.static(path.join(__dirname, 'www')));
-
-
-//returns current set of data for boat
-server.get('/now', function(req, res) {
-    res.send(boatData.now());
-});
-
-
-//TODO: ES library
-var streamConnections = [];
-//on data, send to client
-boatData.on('data', function streamOnData(data) {
-    _.each(streamConnections, function(res) {
-        res.write('id: ' + (new Date().getTime()) + '\n');
-        res.write('data: ' + JSON.stringify(data) + '\n\n');
-    });
-});
-
-//returns json package for each new piece of data on channel
-server.get('/stream', function(req, res) {
-    //let request last forever
-    req.socket.setTimeout(Infinity);
-    
-    //set response type to SSE
-    res.writeHead(200, {
-        'Content-Type': 'text/event-stream',
-        'Cache-Control': 'no-cache',
-        'Connection': 'keep-alive'
-    });
-    res.write('\n');
-
-    streamConnections.push(res);
-
-    //on connection close, remove from list of active streams
-    req.on('close', function() {
-        streamConnections = _.without(streamConnections, res);
-    });
-});
+    addServerDataRoutes(server, boatData);
+    addServerIndexPage(server, settings.get('boatName'), apps);
+}, 35000);
 
 
 
-//load installed apps
-var apps = loadApps(server, boatData, settings);
-
-//used to make list of loaded apps on index page
-server.get('/', function(req, res) {
-    //todo: consider using express's template engine for this.
-    var index = handlebars.compile(fs.readFileSync(path.join(__dirname,'www/templates/index.html'), {encoding:'utf8'}));
-    
-    res.send( index({boatName: settings.get('boatName'), links: apps}) );
-});
-
-
-// start up webserver
-server.set('port', settings.get('port'));
-var server = server.listen(server.get('port'), function() {
-    winston.info('Express server listening on port ' + server.address().port);
-});
-
-// final random stuff
-if ( settings.get('syncSystemTime') ) {
-    function setSystemTime() {
-        boatData.once('data:rmc', function(data) {
-            var now = data['time'];
-            exec('date +%s -s "@' + now.unix() + '"' );
-
-            // fs.exists('/etc/timestamp', function (exists) {
-            //     if (exists) {
-            //         exec('echo "' + now.format("YYYYMMDDmmhhss") +'" >>/etc/timestamp' );
-            //     }
-            // });
-            
-        });
-    };
-
-    //NOTE: changing the system time can give node JS troubles, especially changing it to the past
-    //after some seconds, to give all serial ports time to establish their connections
-    //change the system time to match the GPS provided time
-    setTimeout(setSystemTime, 30000);
-}
